faccbe7f0e160a2f8cc3973b611f4c612a079683
raktimproloy <raktimproloy@gmail.com>
2025-05-28 11:30:11 +0600

    remove path

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 
 import './App.css'
 import Home from './pages/Home'
-import About from './pages/About';
 import { Outlet, useRoutes } from "react-router-dom";
 
 function App() { 
   let routes = useRoutes([
-    { path: "/portfolio", element: <Home /> },
-    { path: "/about", element: <About /> }
+    { path: "/portfolio", element: <Home /> }
   ]);
   return (
     <>
---
+++ src/pages/Test.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Test() {
+  return (
+    <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, impedit? Sed, est. Nobis molestiae sit enim ex nostrum excepturi id!</div>
+  )
+}
